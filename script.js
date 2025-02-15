149f00a61b730c12c0b394c815910c808dc908ee
ADIL  MIRZA <adil.mirza2024@nst.rishihood.edu.in>
2025-02-15 18:00:05 +0530

    canvas design yet to be made

--- script.js
+++ script.js
@@ -1,61 +1,77 @@
-function loco(){
-  // js scrolltrigger aur locomotive ek sath nai chalte hai isliye ek jugaad hai "locomotive scrolltrigger codepen" -- iska js code copy karke paste kar diya
+function loco() {
+  gsap.registerPlugin(ScrollTrigger);
 
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
 
+  // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector("#main").style.transform
+      ? "transform"
+      : "fixed",
+  });
 
-gsap.registerPlugin(ScrollTrigger);
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
-// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector("#main"),
-  smooth: true
-});
-// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-locoScroll.on("scroll", ScrollTrigger.update);
-
-// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
-ScrollTrigger.scrollerProxy("#main", {
-  scrollTop(value) {
-    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-  getBoundingClientRect() {
-    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-  },
-  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
-});
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
 }
+loco();
 
-loco()
+// text animation--
+var tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: `#page1`,
+    start: `top top`,
+    scrub: 1,
+    scroller: `#main`,
+    pin: true,
+  },
+});
 
+// page1 animation--
+tl.to("#page1 #saman,#page1 button", {
+  top: `-95%`,
+});
+
+
+
+
+
+
+// page2 animation--
 
 var tl = gsap.timeline({
-  ScrollTrigger:{
-    trigger:`#page1`,
-    start:`top top`,
-    scrub:1,
-    scroller:`#main`,
-    pin:true,
-  }
-})
+  scrollTrigger: {
+    trigger: `#page3`,
+    start: `top top`,
+    scrub: 1,
+    scroller: `#main`,
+    pin: true,
+  },
+});
 
-tl.to("#page1 para", {
-  top :`-50%`
-})
-
-
-// var tl = gsap.timeline({
-//   ScrollTrigger:{
-//     trigger:"#page2",
-//     start:"top top",
-//     scrub:1,
-//     scroller:"#main",
-//     pin:true,
-//   }
-// })
-// tl.to("#page2 h1", {
-//   top :"-50%"
-// })
-  
+tl.to("#page3 h1 ,#page3 h5", {
+  top: `-50%`,
+});
